ba73ab18d34001b176c298458f97bdf525974ab4
Mondra <mondra73@gmail.com>
2026-01-08 16:13:44 -0300

    fix titulo google

--- src/components/RegisterView.vue
+++ src/components/RegisterView.vue
@@ -128,9 +128,6 @@
               <div class="absolute inset-0 flex items-center">
                 <span class="w-full border-t border-border"></span>
               </div>
-              <div class="relative flex justify-center text-xs uppercase">
-                <span class="bg-background px-2 text-muted-foreground">O regístrate con</span>
-              </div>
             </div>
 
 
